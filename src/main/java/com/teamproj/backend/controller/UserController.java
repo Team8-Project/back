e62097d30cf25e1e6bd243a0a5682c670533ad17
Zabee52 <zabeee52@gmail.com>
2021-12-21 09:26:22 +0900

    Create UserController.java

---
+++ src/main/java/com/teamproj/backend/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.teamproj.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+}
