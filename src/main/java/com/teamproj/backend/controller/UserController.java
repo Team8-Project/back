1e9893ff997e2761879a3527dbf68fd1c659d191
Zabee52 <zabeee52@gmail.com>
2021-12-21 15:51:14 +0900

    회원가입 기능 Controller에 추가
    했어요

--- src/main/java/com/teamproj/backend/controller/UserController.java
+++ src/main/java/com/teamproj/backend/controller/UserController.java
@@ -2,18 +2,26 @@ package com.teamproj.backend.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.teamproj.backend.dto.kakao.KakaoUserResponseDto;
+import com.teamproj.backend.dto.user.signUp.SignUpRequestDto;
+import com.teamproj.backend.dto.user.signUp.SignUpResponseDto;
 import com.teamproj.backend.service.KakaoUserService;
+import com.teamproj.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final KakaoUserService kakaoUserService;
+    private final UserService userService;
+
+    @PostMapping("/api/signup")
+    public ResponseEntity<SignUpResponseDto> signup(@RequestBody SignUpRequestDto signUpRequestDto){
+        return ResponseEntity.ok()
+                .body(userService.signUp(signUpRequestDto));
+    }
 
     @GetMapping("/api/user/kakao/callback")
     public ResponseEntity<KakaoUserResponseDto> kakaoLogin(@RequestParam String code) throws JsonProcessingException {
