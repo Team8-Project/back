daaaa8dd32dfd67cecb5e3f346518c45c966ba7f
Zabee52 <zabeee52@gmail.com>
2021-12-21 16:19:45 +0900

    BoardService 다듬기
    아직 작업중입니다.

--- src/main/java/com/teamproj/backend/Repository/board/BoardRepository.java
+++ src/main/java/com/teamproj/backend/Repository/board/BoardRepository.java
@@ -1,7 +1,12 @@
 package com.teamproj.backend.Repository.board;
 
 import com.teamproj.backend.model.board.Board;
+import com.teamproj.backend.model.board.BoardCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    Optional<List<Board>> findAllByBoardCategory(BoardCategory boardCategory);
 }
--- src/main/java/com/teamproj/backend/service/BoardService.java
+++ src/main/java/com/teamproj/backend/service/BoardService.java
@@ -1,23 +1,37 @@
 package com.teamproj.backend.service;
 
+import com.teamproj.backend.Repository.board.BoardCategoryRepository;
 import com.teamproj.backend.Repository.board.BoardRepository;
 import com.teamproj.backend.dto.board.BoardResponseDto;
 import com.teamproj.backend.model.board.Board;
+import com.teamproj.backend.model.board.BoardCategory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
     private final BoardRepository boardRepository;
+    private final BoardCategoryRepository boardCategoryRepository;
 
-    public List<BoardResponseDto> getBoard() {
-        List<Board> boardList = boardRepository.findAll();
+    public List<BoardResponseDto> getBoard(String category) {
+        Optional<BoardCategory> boardCategory = boardCategoryRepository.findById(category.toUpperCase());
+        if(!boardCategory.isPresent()){
+            throw new NullPointerException("유효한 카테고리가 아닙니다.");
+        }
+
+        Optional<List<Board>> boardList = boardRepository.findAllByBoardCategory(boardCategory.get());
+        if(!boardList.isPresent()){
+            return new ArrayList<>();
+        }
+
         List<BoardResponseDto> boardResponseDtoList = new ArrayList<>();
-        for (Board board : boardList) {
+        for (Board board : boardList.get()) {
             boardResponseDtoList.add(BoardResponseDto.builder()
                     .postId(board.getPostId())
                     .nickname(board.getUser().getNickname())
@@ -26,6 +40,7 @@ public class BoardService {
                     .createdAt(board.getCreatedAt().toLocalDate())
                     .build());
         }
+
         return boardResponseDtoList;
     }
 }
