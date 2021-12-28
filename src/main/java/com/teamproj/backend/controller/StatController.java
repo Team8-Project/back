9a24dffd36efc2b65a4000db8e994bf3b1e7cdc2
Zabee52 <zabeee52@gmail.com>
2021-12-29 03:02:53 +0900

    [Stat] 방문자 수 통계를 위한 엔티티 생성 및 스케줄러 정의

--- src/main/java/com/teamproj/backend/controller/StatController.java
+++ src/main/java/com/teamproj/backend/controller/StatController.java
@@ -2,10 +2,10 @@ package com.teamproj.backend.controller;
 
 import com.teamproj.backend.dto.ResponseDto;
 import com.teamproj.backend.service.StatService;
-import com.teamproj.backend.util.StatisticsUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,8 +22,9 @@ public class StatController {
                 .data(statService.statVisitor())
                 .build();
     }
-    @GetMapping("/stat/quiz")
-    public ResponseDto<Object> statQuizSolver(@RequestParam String category,
+
+    @GetMapping("/stat/quiz/{category}")
+    public ResponseDto<Object> statQuizSolver(@PathVariable String category,
                                               @RequestParam int score){
         statService.statQuizSolver(category, score);
         return ResponseDto.builder()
