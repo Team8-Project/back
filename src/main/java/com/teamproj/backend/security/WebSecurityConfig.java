3d41a74e69f7ff6066a671f7e583ff90e7ec12f5
Zabee52 <zabeee52@gmail.com>
2021-12-29 22:02:33 +0900

    [Dict] 사전 총 갯수 출력 기능

--- src/main/java/com/teamproj/backend/security/WebSecurityConfig.java
+++ src/main/java/com/teamproj/backend/security/WebSecurityConfig.java
@@ -134,6 +134,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         skipPathList.add("GET,/api/dict/*/history");
         skipPathList.add("GET,/api/dict/history/*");
         skipPathList.add("GET,/api/searchInfo/dict");
+        skipPathList.add("GET,/api/count/dict");
         // 게시판 목록 API 허용
         skipPathList.add("GET,/api/board/subject");
         skipPathList.add("GET,/api/board/**");
