f222b04f02ecd181bf94e0bd3bf36175e6eb2d1b
naocya@naver.com <naocya@naver.com>
2022-01-05 16:58:18 +0900

    Bug/git

--- src/main/java/com/teamproj/backend/service/BoardHashTagService.java
+++ src/main/java/com/teamproj/backend/service/BoardHashTagService.java
@@ -1,5 +1,4 @@
 package com.teamproj.backend.service;
-
 import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.teamproj.backend.dto.board.BoardHashTag.BoardHashTagResponseDto;
@@ -9,19 +8,15 @@ import com.teamproj.backend.util.MySqlJpaTemplates;
 import com.teamproj.backend.util.RedisKey;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
 import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 public class BoardHashTagService {
     private final RedisService redisService;
-
     private final EntityManager entityManager;
-
     //region 해시태그 추천
     public BoardHashTagResponseDto getRecommendHashTag() {
         List<String> recommendHashTagStrList = redisService.getRecommendHashTag(RedisKey.HASHTAG_RECOMMEND_KEY);
@@ -35,7 +30,6 @@ public class BoardHashTagService {
                     .orderBy(NumberExpression.random().asc())
                     .limit(20)
                     .fetch();
-
             redisService.setRecommendHashTag(RedisKey.HASHTAG_RECOMMEND_KEY, boardHashTagList);
             resultHashTagStrList = redisService.getRecommendHashTag(RedisKey.HASHTAG_RECOMMEND_KEY);
         }
