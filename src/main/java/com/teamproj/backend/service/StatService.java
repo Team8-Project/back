1575ab518fa697d9e3143771af52a33c1bce2f35
Zabee52 <zabeee52@gmail.com>
2022-01-10 16:22:29 +0900

    [Stat] 게시글 수정시 수정 목록에 반영 되도록 수정

--- src/main/java/com/teamproj/backend/service/StatService.java
+++ src/main/java/com/teamproj/backend/service/StatService.java
@@ -1,12 +1,11 @@
 package com.teamproj.backend.service;
 
+import com.teamproj.backend.Repository.stat.StatBoardModifyRepository;
 import com.teamproj.backend.Repository.stat.StatQuizSolverRepository;
 import com.teamproj.backend.Repository.stat.StatQuizStarterRepository;
 import com.teamproj.backend.Repository.stat.StatVisitorRepository;
-import com.teamproj.backend.model.statistics.StatNumericData;
-import com.teamproj.backend.model.statistics.StatQuizSolver;
-import com.teamproj.backend.model.statistics.StatQuizStarter;
-import com.teamproj.backend.model.statistics.StatVisitor;
+import com.teamproj.backend.model.board.Board;
+import com.teamproj.backend.model.statistics.*;
 import com.teamproj.backend.util.StatisticsUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,15 +19,29 @@ public class StatService {
     private final StatVisitorRepository statVisitorRepository;
     private final StatQuizStarterRepository statQuizStarterRepository;
     private final StatQuizSolverRepository statQuizSolverRepository;
+    private final StatBoardModifyRepository statBoardModifyRepository;
+
+    // 게시글 수정 내역
+    public void statBoardModify(Board board){
+        StatBoardModify statBoardModify = StatBoardModify.builder()
+                .boardId(board.getBoardId())
+                .build();
+        statBoardModifyRepository.save(statBoardModify);
+    }
 
     // 일일 방문자 통계
     // 하루 지나면 삭제됩니다.
-    // 원래 데이터 전부 쌓아놓고 싶었는데 용량을 고려하여..
+    // 원래 데이터 전부 쌓아놓고 싶었는데 용량과 속도를 고려하여.....
     public long statVisitor() {
-        StatVisitor statVisitor = getSaveStatVisitorByVisitorIp(StatisticsUtils.getClientIp(), StatisticsUtils.getClientReferer());
-        statVisitorRepository.save(statVisitor);
+        StatVisitor statVisitor = getSafeStatVisitorByVisitorIp(StatisticsUtils.getClientIp(), StatisticsUtils.getClientReferer());
+        statVisitorRepository.save(statVisitor); // modifiedAt(최근 방문시간) 기록하기 위해
 
         return statVisitorRepository.count();
+    }
+
+    // 퀴즈 시작한 사람 숫자
+    public long getStatQuizStarter(){
+        return statQuizStarterRepository.count();
     }
 
     // 퀴즈 시작한사람 통계
@@ -58,7 +71,7 @@ public class StatService {
 
     // GetSafeEntity
     // StatVisitor
-    private StatVisitor getSaveStatVisitorByVisitorIp(String visitorIp, String referer) {
+    private StatVisitor getSafeStatVisitorByVisitorIp(String visitorIp, String referer) {
         Optional<StatVisitor> statVisitor = statVisitorRepository.findByVisitorIp(visitorIp);
         return statVisitor.orElseGet(() -> StatVisitor.builder()
                 .visitorIp(visitorIp)
