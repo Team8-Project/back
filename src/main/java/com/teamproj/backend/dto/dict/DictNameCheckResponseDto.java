23ab12eeb918f2ed7b6506692a18ff64fc1d3fcc
Zabee52 <zabeee52@gmail.com>
2022-02-21 23:59:19 +0900

    [Dict] 주석 정리 및 기능 개선

--- src/main/java/com/teamproj/backend/dto/dict/DictNameCheckResponseDto.java
+++ src/main/java/com/teamproj/backend/dto/dict/DictNameCheckResponseDto.java
@@ -1,5 +1,6 @@
 package com.teamproj.backend.dto.dict;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class DictNameCheckResponseDto {
     private boolean result;
 }
